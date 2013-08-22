9fc2053e6b639e162f0d311b17609b594bb2894b
Cassie <ceassi@yahoo.com>
2013-08-22 23:25:06 +0800

    Update gitignore, login screen

--- Pasahero/src/com/example/pasahero/AuthActivity.java
+++ Pasahero/src/com/example/pasahero/AuthActivity.java
@@ -1,13 +1,25 @@
 package com.example.pasahero;
 
-public class AuthActivity {
+import roboguice.activity.RoboActivity;
+import roboguice.inject.InjectView;
+import android.graphics.Typeface;
+import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
-	/**
-	 * @param args
-	 */
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+public class AuthActivity extends RoboActivity{
 
+	@InjectView(R.id.et_emailAddress) EditText emailAddress;
+	@InjectView(R.id.et_password) EditText password;
+	@InjectView(R.id.b_okay) Button okay;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		Typeface font = Typefaces.get(this, Config.FONTAWESOME);
+		setContentView(R.layout.activity_auth);
+		okay.setTypeface(font);
 	}
-
+	
+	
 }
--- Pasahero/src/com/example/pasahero/MainActivity.java
+++ Pasahero/src/com/example/pasahero/MainActivity.java
@@ -1,7 +1,8 @@
 package com.example.pasahero;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
 import android.view.Menu;
 
 public class MainActivity extends Activity {
@@ -10,6 +11,8 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		startActivity(new Intent(getApplicationContext(),
+				AuthActivity.class));
 	}
 
 	@Override
