102e8d4e11ff185c6dfab02744410094b42395f8
Cassie <ceassi@yahoo.com>
2013-08-26 21:09:21 +0800

    Signing up

--- backend/config.py
+++ backend/config.py
@@ -1,3 +1,5 @@
+from datetime import timedelta
+
 DBNAME = 'pasahero'
 DEBUG = True
 SECRET_KEY = '\x18a8\x9e\x15\x94s\x13LD\x08\xa2,l\xc9\xc5\xe4\xab\x7f\x04\x91\x0b\x02i'
@@ -5,3 +7,5 @@ USERNAME = 'admin'
 PASSWORD = 'password'
 DBHOST = 'localhost'
 DBPORT = 28015
+REMEMBER_COOKIE_DURATION = timedelta(days=14)
+TIMEZONE = 'Asia/Manila'
