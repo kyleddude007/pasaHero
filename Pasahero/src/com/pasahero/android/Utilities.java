ca8c14af533d8cd99785874cea77c78463d1788b
Cassie <ceassi@yahoo.com>
2013-09-07 23:16:14 +0800

    Added opentripplanner stuff

--- Pasahero/src/com/pasahero/android/Utilities.java
+++ Pasahero/src/com/pasahero/android/Utilities.java
@@ -1,5 +1,10 @@
 package com.pasahero.android;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import org.opentripplanner.util.model.EncodedPolylineBean;
+
 public class Utilities {
 
 	public static Object ifExists(Object object){
@@ -9,4 +14,5 @@ public class Utilities {
 			return null;
 		}
 	}
+
 }
